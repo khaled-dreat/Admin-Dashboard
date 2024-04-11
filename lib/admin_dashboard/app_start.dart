0c381b7e7d1c2dff8ede95f49fab00c6fbc30b19
khaled-dreat <dreatkhaled@gmail.com>
2024-04-11 08:46:54 +0300

    Refactor main.dart to use AppStart widget
    Remove MyApp and MyHomePage classes, refactor main.dart to use the new AppStart widget for application startup.

--- lib/admin_dashboard/app_start.dart
+++ lib/admin_dashboard/app_start.dart
@@ -1,1 +1,15 @@
 part of "../core/import/app_import.dart";
+
+class AppStart extends StatelessWidget {
+  const AppStart({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
