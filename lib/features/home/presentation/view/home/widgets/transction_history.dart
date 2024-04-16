a48ef04ab39b8ccb733c08756d95d66c7e2e7a5e
khaled-dreat <dreatkhaled@gmail.com>
2024-04-16 07:35:38 +0300

    Add TransctionHistoryHeader widget to TransctionHistory widget
    Add TransctionHistoryHeader widget to TransctionHistory widget for displaying
    transaction history header in the UI.

--- lib/features/home/presentation/view/home/widgets/transction_history.dart
+++ lib/features/home/presentation/view/home/widgets/transction_history.dart
@@ -6,6 +6,28 @@ class TransctionHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      children: [
+        const TransctionHistoryHeader(),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          "13 April 2022",
+          style: AppStyles.styleRegular16(context),
+        )
+      ],
+    );
+  }
+}
+
+class TransctionHistoryHeader extends StatelessWidget {
+  const TransctionHistoryHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
