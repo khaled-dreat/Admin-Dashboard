900f9066713ba7540725f13ec22e37a6aa986be2
khaled-dreat <dreatkhaled@gmail.com>
2024-04-21 20:54:28 +0300

    Add conditional rendering for app bar based on screen width

--- lib/features/home/presentation/view/home/home_view.dart
+++ lib/features/home/presentation/view/home/home_view.dart
@@ -6,6 +6,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: MediaQuery.sizeOf(context).width < 800 ? AppBar() : null,
       backgroundColor: const Color(0xFFF7F9FA),
       body: AdaptiveLayout(
           mbileLayoutView: (context) => const DashboardMobileLayout(),
